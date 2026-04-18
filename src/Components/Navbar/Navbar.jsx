1157073dd644dd24563f45b733fac4f69c4a72fb
tfshorifulislam <tfshorifulislam@gmail.com>
2026-04-18 17:00:51 +0600

    navbar and banner design done

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,35 +1,37 @@
 import Link from 'next/link';
 import React from 'react';
+import { AiOutlineHome } from "react-icons/ai";
+import { RiTimeLine } from "react-icons/ri";
+import { ImStatsDots } from "react-icons/im";
 
 const Navbar = () => {
     return (
-        <div className="navbar bg-base-100 shadow-sm">
-            <div className="navbar-start">
-                <div className="dropdown">
-                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+        <div className="bg-base-100 shadow-sm">
+            <div className='flex justify-between navbar w-11/12 mx-auto'>
+                <div>
+                    <div className="dropdown">
+                        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+                        </div>
+                        <ul
+                            tabIndex="-1"
+                            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+                            <li><Link href={'/'}>Home</Link></li>
+                            <li><Link href={'timeline'}>Timeline</Link></li>
+                            <li><Link href={'stats'}>Stats</Link></li>
+
+                        </ul>
                     </div>
-                    <ul
-                        tabIndex="-1"
-                        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-                        <li><Link href={'/'}>Home</Link></li>
-                        <li><Link href={'timeline'}>Timeline</Link></li>
-                        <li><Link href={'stats'}>Stats</Link></li>
+                    <Link href={'/'} className="cursor-pointer text-sm md:text-xl">KeenKeeper</Link>
+                </div>
+                <div className="hidden lg:flex">
+                    <ul className="menu menu-horizontal px-1 gap-5 text-lg">
+                        <li><Link href={'/'}><AiOutlineHome /> Home</Link></li>
+                        <li><Link href={'timeline'}><RiTimeLine /> Timeline</Link></li>
+                        <li><Link href={'stats'}><ImStatsDots /> Stats</Link></li>
 
                     </ul>
                 </div>
-                <Link href={'/'} className="cursor-pointer text-sm md:text-xl">KeenKeeper</Link>
-            </div>
-            <div className="navbar-center hidden lg:flex">
-                <ul className="menu menu-horizontal px-1">
-                    <li><Link href={'/'}>Home</Link></li>
-                    <li><Link href={'timeline'}>Timeline</Link></li>
-                    <li><Link href={'stats'}>Stats</Link></li>
-
-                </ul>
-            </div>
-            <div className="navbar-end">
-                <a className="btn btn-xs sm:btn-sm md:btn-md ">Button</a>
             </div>
         </div>
     );
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,62 @@
 import React from 'react';
+import { FiPlus } from "react-icons/fi";
 
 const MainPage = () => {
     return (
-        <div>
-            
+        <div className='w-11/12 mx-auto'>
+            <div className='space-y-4 mb-8 flex flex-col mt-20 justify-center'>
+                <h1 className='font-bold text-4xl text-[#1F2937] text-center'>
+                    Friends to keep close in your life
+                </h1>
+                <p className='text-center text-[#64748B]'>
+                    Your personal shelf of meaningful connections. Browse, tend, and nurture the
+                    relationships that matter most.
+                </p>
+            </div>
+            <div className='flex justify-center mb-10'>
+                <button className='btn text-white font-semibold bg-[#244D3F] rounded-sm flex justify-center items-center'>
+                    <FiPlus />
+                    Add a Friend
+                </button>
+            </div>
+
+            <div className='grid grid-cols-2 md:grid-cols-4 gap-10 mb-10'>
+                <div className='bg-base-100 rounded-lg shadow-sm space-y-2 py-8'>
+                    <p className='text-3xl text-center font-semibold text-[#244d3f]'>
+                        10
+                    </p>
+                    <p className='text-center text-[#64748B]'>
+                        Total Friends
+                    </p>
+                </div>
+                <div className='bg-base-100 rounded-lg shadow-sm space-y-2 py-8'>
+                    <p className='text-3xl text-center font-semibold text-[#244d3f]'>
+                        3
+                    </p>
+                    <p className='text-center text-[#64748B]'>
+                        On Track
+                    </p>
+                </div>
+                <div className='bg-base-100 rounded-lg shadow-sm space-y-2 py-8'>
+                    <p className='text-3xl text-center font-semibold text-[#244d3f]'>
+                        6
+                    </p>
+                    <p className='text-center text-[#64748B]'>
+                        Need Attention
+                    </p>
+                </div>
+                <div className='bg-base-100 rounded-lg shadow-sm space-y-2 py-8'>
+                    <p className='text-3xl text-center font-semibold text-[#244d3f]'>
+                        12
+                    </p>
+                    <p className='text-center text-[#64748B]'>
+                        Interactions This Month
+                    </p>
+                </div>
+            </div>
+            <div className="flex w-full flex-col mb-10">
+                <div className="divider"></div>
+            </div>
         </div>
     );
 };

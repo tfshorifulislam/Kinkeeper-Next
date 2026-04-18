362cd282aab934de09f6169b5df9f12672099940
tfshorifulislam <tfshorifulislam@gmail.com>
2026-04-18 18:37:19 +0600

    all friends data show dynamicly

--- src/Components/CreateFriendCards/CreateFriendCards.jsx
+++ src/Components/CreateFriendCards/CreateFriendCards.jsx
@@ -3,14 +3,40 @@ import React from 'react';
 
 const CreateFriendCards = ({ friendsData }) => {
     return (
-        <div className='bg-base-100 p-6 flex flex-col justify-center items-center'>
+        <div className='bg-base-100 shadow-md rounded-md p-6 flex flex-col justify-center items-center'>
             <div>
                 <Image
-                    width={80}
-                    height={80}
+                    className='rounded-full mb-3'
+                    width={100}
+                    height={100}
                     src={friendsData.picture} alt={friendsData.name} />
             </div>
-        </div>
+            <div className='space-y-2 text-center'>
+                <p className='font-semibold text-xl text-[#1F2937] '>
+                    {friendsData.name}
+                </p>
+                <p className='text-[12px] text-[#64748B]'>
+                    {friendsData.days_since_contact}d ago
+                </p>
+                <p className='flex flex-col md:flex-row gap-2 justify-center text-[#244D3F] font-medium text-[12px]'>
+                    {
+                        friendsData.tags.map((tag, i) => <span key={i}
+                            className='bg-[#CBFADB] py-2 px-6 rounded-full'>
+                            {tag}
+                        </span>)
+                    }
+                </p>
+                <div
+                    className={` rounded-full py-2 text-white font-medium text-[12px]
+                ${friendsData.status === 'overdue' ? 'bg-[#EF4444]' :
+                            friendsData.status === 'almost due' ? 'bg-[#EFAD44]' :
+                                friendsData.status === 'on-track' ? 'bg-[#244D3F]' : ''
+                        }`
+                    }>
+                    {friendsData.status}
+                </div>
+            </div>
+        </div >
     );
 };
 
